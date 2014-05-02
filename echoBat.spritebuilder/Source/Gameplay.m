57db8932184c919e1c4f50cfe62720239c37c480
Jordan Arnesen <jordan101112@gmail.com>
2014-05-02 15:01:47 -0700

    adding init values

--- echoBat.spritebuilder/Source/Gameplay.m
+++ echoBat.spritebuilder/Source/Gameplay.m
@@ -14,6 +14,37 @@
     NSArray *soundFiles;
     NSArray *soundNames;
     NSDictionary *soundEffects;
+    
+//    Bat pred;
+//    Bat prey;
+//    
+//    SoundWave pulse;
+//    SoundWave echo;
+    
+    float speed; // speed of bat
+    float earFact; // factor divided by speed to get "ear size"
+    int size; // bat and target size
+    
+    int caught; // timer to display extra text after each capture
+    int speedCh; // timer to display change of bat speed txt
+    int pulseCh; // timer to display change of pulse speed txt
+    
+    int timeElapsed; // milliseconds since start of program
+    int gameDur;  // length of game in seconds
+    int timerStart;  // for setting begining of timer
+    int startPause;
+    int timerPaused; // sum total of time in paused/lost mode
+    int timer;  // current in-game timer
+    
+    int pulseSpeed; // speed of pulses (aka speed of sound in game)
+    bool bounced; // has the current pulse generated an echo yet?
+    bool heardR; // has right ear heard current echo?
+    bool heardL; // has left ear heard current echo?
+    
+    
+    BOOL start;
+    BOOL paused;
+    BOOL gameOver;
 
 }
 
@@ -22,6 +53,25 @@
 
     if (self) {
         // initialize instance variables here
+        caught = 0; // timer to display extra text after each capture
+        speedCh = 0; // timer to display change of bat speed txt
+        pulseCh = 0; // timer to display change of pulse speed txt
+        
+        timeElapsed; // milliseconds since start of program
+        gameDur = 180;  // length of game in seconds
+        timerStart;  // for setting begining of timer
+        startPause;
+        timerPaused = 0; // sum total of time in paused/lost mode
+        timer = gameDur;  // current in-game timer
+        
+        pulseSpeed = 10; // speed of pulses (aka speed of sound in game)
+        bounced = false; // has the current pulse generated an echo yet?
+        heardR = false; // has right ear heard current echo?
+        heardL = false; // has left ear heard current echo?
+        
+        start = NO;
+        paused = false;
+        gameOver = false;
         
         //create audio player and preload sound effects
         player = [OALSimpleAudio sharedInstance];
@@ -50,6 +100,8 @@
         
         soundEffects = [NSDictionary dictionaryWithObjects:soundFiles forKeys:soundNames];
         
+        
+        start = YES;
         
         // finished initializing
         CCLOG(@"init");
